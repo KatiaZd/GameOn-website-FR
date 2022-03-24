31ddd2311d93b66db80fee53d91cae0774ed64ab
KatiaZd <katia.zouad@hotmail.fr>
2022-03-24 18:20:18 +0100

    Input

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,25 +1,27 @@
 /* AFFECTATION DES CONSTANTES AU DOM */
 
-const modalbg = document.querySelector(".bground");           // Modale formulaire
-const modalBtn = document.querySelectorAll(".modal-btn");     // Bouton "Je m'inscrit"
-const formData = document.querySelectorAll(".formData");
-const closeModal = document.querySelector(".close");          // Fermeture de la modale
-const firstName = document.querySelector("input[name=first]");
-const lastName = document.querySelector("input[name=last]");
-const email = document.querySelector("input[name=email]");
-const birthdate = document.querySelector("input[name=birthdate]");
-const quantity = document.querySelector("input[name=quantity]");
-const city = document.querySelector("input[type=radio]");       
-const user = document.querySelector("input[type=checkbox]");
-const modalMessageThanks = document.querySelector("#messageThanks");  // Modale finale de remerciement
-const closeThanks = document.querySelector(".closeThanksBtn");
-//const formData = document.querySelectorAll(".formData");
+const modalbg             = document.querySelector(".bground");               // Modale formulaire
+const form                = document.querySelector("form[name=reserve]");     // Corps du formulaire
+const modalBtn            = document.querySelectorAll(".modal-btn");          // Bouton "Je m'inscrit"
+const formData            = document.querySelectorAll(".formData");           // Data-error
+const closeModal          = document.querySelector(".close");                 // Fermeture de la modale
+const firstName           = document.querySelector("input[name=first]");
+const lastName            = document.querySelector("input[name=last]");
+const email               = document.querySelector("input[name=email]");
+const birthdate           = document.querySelector("input[name=birthdate]");
+const quantity            = document.querySelector("input[name=quantity]");
+const city                = document.querySelector("input[type=radio]");       
+const user                = document.querySelector("input[type=checkbox]");
+const modalMessageThanks  = document.querySelector("#messageThanks");           // Modale finale de remerciement
+const closeThanks         = document.querySelector(".closeThanksBtn");          // Bouton "Fermer", modale final
+
+
 
 
 /* ECOUTE DES EVENEMENTS */
 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));  // Ouverture de la modale
-closeModal.addEventListener("click", closeFormModal); // Fermeture de la modale au click
+closeModal.addEventListener("click", closeFormModal);                   // Fermeture de la modale au click
 firstName.addEventListener("input", firstNameValidation);
 lastName.addEventListener("input", lastNameValidation);
 email.addEventListener("input", emailValidation);
@@ -27,7 +29,8 @@ birthdate.addEventListener("input", birthdateValidation);
 quantity.addEventListener("input", quantityValidation);
 city.addEventListener("radio", cityValidation);
 user.addEventListener("input", userValidation);
-modalMessageThanks.addEventListener("click", modalThanks);
+closeThanks.addEventListener("click", closeFormModal);
+
 
 
 /* FONCTIONS OUVERTURE/FERMETURE DE LA MODALE */
@@ -61,21 +64,21 @@ function editNav() {
 function firstNameValidation() {
   let regexName = /^[A-Za-zÀ-ÿ-']{2,20}$/;
 
-  if (firstName.value >=2 ) {
-    firstName.classList.add("input-error");
-    firstName.classList.remove("input-validate");
+  if (firstName.value <=1 ) {
+    firstName.classList.add("inputError");
+    firstName.classList.remove("inputValidate");
     firstNameError.innerHTML = "Veuillez saisir 2 caractères minimum";
     return false;
   } 
   else if (regexName.test(firstName.value) === false) { 
-    firstName.classList.add("input-error");
-    firstName.classList.remove("input-validate");
-    firstNameError.innerHTML = "Format incorrect";
+    firstName.classList.add("inputError");
+    firstName.classList.remove("inputValidate");
+    firstNameError.innerHTML = "Veuillez saisir 2 caractères minimum";
     return false;
   } 
   else {
-    firstName.classList.remove("input-error");
-    firstName.classList.add("input-validate");
+    firstName.classList.remove("inputError");
+    firstName.classList.add("inputValidate");
     firstNameError.innerHTML = "";
     return true;
   }
@@ -85,21 +88,21 @@ function firstNameValidation() {
 function lastNameValidation() {
   let regexName = /^[A-Za-zÀ-ÿ-']{2,20}$/;
 
-  if (lastName.value >=2) {
-    lastName.classList.add("input-error");
-    lastName.classList.remove("input-validate");
+  if (lastName.value <=1) {
+    lastName.classList.add("inputError");
+    lastName.classList.remove("inputValidate");
     lastNameError.innerHTML = "Veuillez saisir 2 caractères minimum";
     return false;
   } 
-  else if (regexName.test(lastName.value) === false) {    
-    lastName.classList.add("input-error");
-    lastName.classList.remove("input-validate");
+  else if (regexName.test(lastName.value) === false) { 
+    lastName.classList.add("inputError");
+    lastName.classList.remove("inputValidate");   
     lastNameError.innerHTML = "Format incorrect";
     return false;
   } 
   else {
-    lastName.classList.remove("input-error");
-    lastName.classList.add("input-validate");
+    lastName.classList.remove("inputError");
+    lastName.classList.add("inputValidate");
     lastNameError.innerHTML = "";
     return true;
   }
@@ -107,24 +110,23 @@ function lastNameValidation() {
 
 // fonction email
 function emailValidation() {
-  //General email regex, RFC 5322
   let regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
   if (email.value.trim() === "") {
-    email.classList.add("input-error");
-    email.classList.remove("input-validate");
+    email.classList.add("inputError");
+    email.classList.remove("inputValidate");
     emailError.innerHTML = "Veuillez saisir votre adresse mail";
     return false;
   } 
   else if (regexMail.test(email.value) == false) {
-    email.classList.add("input-error");
-    email.classList.remove("input-validate");
+    email.classList.add("inputError");
+    email.classList.remove("inputValidate");
     emailError.innerHTML = "Format incorrect";
     return false;
   } 
   else {
-    email.classList.remove("input-error");
-    email.classList.add("input-validate");
+    email.classList.remove("inputError");
+    email.classList.add("inputValidate");
     emailError.innerHTML = "";
     return true;
   }
@@ -136,15 +138,15 @@ function birthdateValidation() {
   const date1 = new Date("2020-01-01");
   const birthdateUser =  new Date(birthdate.value)
   if (birthdateUser <= date) {  
-    birthdate.classList.add("input-error");
-    birthdate.classList.remove("input-validate");
+    birthdate.classList.add("inputError");
+    birthdate.classList.remove("inputValidate");
     birthdateError.innerHTML = "Veuillez saisir une date valide";
     return false;
   }
 
   else {
-    birthdate.classList.remove("input-error");
-    birthdate.classList.add("input-validate");
+    birthdate.classList.remove("inputError");
+    birthdate.classList.add("inputValidate");
     birthdateError.innerHTML = "";
     return true;  
   }
@@ -153,14 +155,14 @@ function birthdateValidation() {
 // fonction quantity
 function quantityValidation() {
   if (((quantity.value === "") != quantity.value <0) !=quantity.value > 99) {
-    quantity.classList.add("input-error");
-    quantity.classList.remove("input-validate");
-    quantityError.innerHTML = "Veuillez choisir un nombre de 0 à 99.";    //Ligne 118 HTML
+    quantity.classList.add("inputError");
+    quantity.classList.remove("inputValidate");
+    quantityError.innerHTML = "Veuillez choisir un nombre de 0 à 99.";    
     return false; 
   }
   else {
-    quantity.classList.remove("input-error");
-    quantity.classList.add("input-validate");
+    quantity.classList.remove("inputError");
+    quantity.classList.add("inputValidate");
     quantityError.innerHTML = "";
     return true;
   }
@@ -168,24 +170,21 @@ function quantityValidation() {
 
 
 // fonction city
-function cityValidation () {
-  console.log(city.value);
-  var checked = false
+function cityValidation() {
+  var checked = false;
   for (let i = 0; i < city.length; i++) {
     if (city[i].checked) {
       checked = true;
-      cityError.innerHTML = "Vous avez choisi" + city[i].value;
     }
   }
-  if (checked == false); {     
+  if (checked == false) {     
     cityError.innerHTML = "Veuillez choisir une ville";
   
   }
 }
 
-
 // fonction conditions d'utilisation
-function userValidation (){
+function userValidation(){
   if (!user.checked) {
     checkedConditionError.innerHTML = "Pour participer au tournois, veuillez accepter les conditions d'utilisation.";
     return false;
@@ -199,18 +198,18 @@ function userValidation (){
 // fonction formulaire si tout est ok
 
 function validate() {
-  let validate = true;
+  var validation = true;
 
   if (!firstNameValidation()||!lastNameValidation()||!emailValidation()||
-  !birthdateValidation()||!quantityValidation()||cityValidation()||!userValidation()) { 
+  !birthdateValidation()||!quantityValidation()||!cityValidation()||!userValidation()) { 
     validation = false;
   }
 
   if (validation === true) {
-    formError.innerHTML = "";
-    form.reset();
-    form.style.display = "none";
-    modalThanks();
+    formError.innerHTML = "";         // Validation du form. = pas de message d'erreur
+    form.reset();                     // Le form. se réinitialise (= s'efface)
+    form.style.display = "none";      // Fermeture de la modale
+    modalThanks();                    // Modale de remerciement s'affiche
   } 
   else{
     formError.innerHTML = "Veuillez renseigner tous les champs";
